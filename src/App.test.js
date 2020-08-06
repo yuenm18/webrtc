75095b182e333dfd60aa7294aa477eb14c8df0ec
Matthew Yuen <yuenm18@up.edu>
2020-08-06 01:19:08 -0700

    fixed unit test

--- src/App.test.js
+++ src/App.test.js
@@ -2,8 +2,8 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders "Go to this link to join this chat" link', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/learn react/i);
+  const linkElement = getByText(/Go to this link to join this chat/i);
   expect(linkElement).toBeInTheDocument();
 });
